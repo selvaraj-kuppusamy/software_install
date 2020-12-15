dbd5fc659e9a66ec6f212b33139c39d799792a5f
Selvaraj Kuppusamy <68200256+selvaraj-kuppusamy@users.noreply.github.com>
2020-12-15 10:20:55 +0530

    google chrome install in ubuntu

--- chrome_install/chrome_install_in_ubuntu.sh
+++ chrome_install/chrome_install_in_ubuntu.sh
@@ -3,12 +3,16 @@
 #chrome installing in the ubuntu
 #upgrade the system
 echo "hey! Google chrome is installing your system"
+echo "Your system is upgrading!...."
 sudo apt upgrade
 #update the system
+echo "Your system is updating!...."
 sudo apt update
 #download google chrome file in .deb package
+echo "Downloading Google Chrome .deb package file"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 #install google chrome
+echo "Installing Google Chrome .deb package file"
 sudo apt install ./google-chrome-stable_current_amd64.deb
 echo "Google is successfully installed your system"
 echo "Google chrome is opeing soon..."
