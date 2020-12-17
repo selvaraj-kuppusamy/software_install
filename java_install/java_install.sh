642d1bb0a6da0228bcf1eb0a3f698952fb21d56e
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2020-12-17 18:15:30 +0530

    java_install

---
+++ java_install/java_install.sh
@@ -0,0 +1,15 @@
+#/bin/sh
+#upgrade the system
+echo "Your system is Upgrading!..."
+sudo apt upgrade
+#update the system
+echo "Your system is updating!..."
+sudo apt update
+#install java on ubuntu
+#install openJDK
+echo "Installing java on ubuntu!..."
+echo "Install open Java Development Kit..."
+sudo apt install default-jdk
+echo "open Java Development kit is sucessfully installed you system!!!"
+
+
