7e4bd969f6d7c37c51b0be69c2a303cb9fac7ada
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-02-24 20:07:35 +0530

    ngrok install in ubuntu

---
+++ ngrok_install/ngork_install.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#install ngrok in the ubuntu
+#Download ngrok zip file 
+echo "Downloading Ngrok zip file..."
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+#unzip ngrok zip file
+echo "Unzipping the ngrok file!..."
+unzip ngrok-stable-linux-amd64.zip
+echo "ngrok has successfully installed!"
