b89884e3a8093c19433c3bd36d728ea1fef1be13
Selvaraj Kuppusamy <velmanikpvs@gmail.com>
2021-06-10 07:53:03 +0530

    C Complier Install in Ubuntu

--- c_complier_install/c_complier.sh
+++ c_complier_install/c_complier.sh
@@ -5,7 +5,7 @@ echo "Your system is Upgrading!..."
 sudo apt upgrade -y
 #update the system
 echo "Your system is updating!..."
-sudo apt update -y 
+sudo apt update
 #c complier install
 #Note that point if you want install pakages all packages like g++,etc...
 #use this commnad sudo apt install build-essential
