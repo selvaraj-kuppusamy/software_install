4aa44f0b5d13a583f2666d75f69a4c80fdddd4c6
Selvaraj Kuppusamy <velmanikpvs@gmail.com>
2021-06-16 10:40:12 +0530

    Browser Install

--- Browser_Install/browserinstall.sh
+++ Browser_Install/browserinstall.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-#upgrade the system
-echo "Your! system is upgrading!..."
-sudo apt upgrade -y
 #update the system
 echo "Your! system is updating!..."
 sudo apt update
+#upgrade the system
+echo "Your! system is upgrading!..."
+sudo apt upgrade -y
 #choice to install or uninstall browser
 
 case $@ in 
