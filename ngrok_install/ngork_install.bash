d6a1bd992698dc1419a4be92be67c863d55557e3
Selvaraj Kuppusamy <velmanikpvs@gmail.com>
2021-06-16 10:37:25 +0530

    Ngrok Install

--- ngrok_install/ngork_install.bash
+++ ngrok_install/ngork_install.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#upgrade the system
-echo "Your system is Upgrading!..."
-sudo apt upgrade -y
 #update the system
 echo "Your system is updating!..."
 sudo apt update -y 
+#upgrade the system
+echo "Your system is Upgrading!..."
+sudo apt upgrade -y
 #install ngrok in the ubuntu
 #Download ngrok zip file 
 echo "Downloading Ngrok zip file..."
