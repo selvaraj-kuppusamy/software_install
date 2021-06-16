1dcdf2c88ea65b51a76240ce1aa8f661a867da67
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-06-16 12:51:03 +0530

    OpenSSh Install

---
+++ OpenSSH_Install/OpenSSH_Install.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+#update the system
+echo "Your! system is updating!..."
+sudo apt update
+#upgrade the system
+echo "Your! system is upgrading!..."
+sudo apt upgrade -y
+
+#Install Openssh
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "+				OPENSSH				      +"
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "Installing Openssh!...."
+sudo apt install openssh-server
+sudo apt install openssh-client
+#Allow Firewall in ssh
+echo "Allowing firewall!...."
+sudo ufw allow ssh
+#Check Firewall status
+echo "Checking ufw firewall status!..."
+sudo ufw status
+#Enable ssh server
+echo "Enabling ssh service!..."
+sudo systemctl enable ssh
+#Check service Status	
+echo "Checking ssh service Status!...."
+sudo systemctl status sshd --no-pager
+echo "Openssh is successfullt Installed"
