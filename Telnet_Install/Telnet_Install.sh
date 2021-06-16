48b844eef26e51cb2f8852b18721b38535d5475c
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-06-16 08:51:18 +0530

    Telnet Install in ubuntu

---
+++ Telnet_Install/Telnet_Install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+#update the system
+echo "Your! system is updating!..."
+sudo apt update
+#upgrade the system
+echo "Your! system is upgrading!..."
+sudo apt upgrade -y
+#Telnet Installation
+echo "TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT"
+echo "T                            Telnet Install                            T"
+echo "TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT"
+echo "Telnet is Installing!...."
+sudo apt install telnetd -y
+#Allow 23 Port Number
+echo "Allowing Port Number!..."
+sudo ufw allow 23/tcp
+#Check Status
+echo "Checking Telnet Service status!...."
+systemctl status inetd --no-pager
+#reload Ufw Firewall
+echo "Reloading ufw Firewall!..."
+ufw reload
+echo "Telnet is Successfully Installed!"
