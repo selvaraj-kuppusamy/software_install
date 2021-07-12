f9be970b3d2138896fa336932cf4955df0dc9639
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-07-12 23:48:36 +0530

    Putty Install

--- Putty_Install/Putty_Install.sh
+++ Putty_Install/Putty_Install.sh
@@ -1,5 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
+#Need to run this script as a root
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root (with sudo command)"
+  exit
+fi
+#Putty Install
 #update the system
 echo "Your system is updating!..."
 sudo apt update
